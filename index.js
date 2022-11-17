b135bea570b116de2276faf3fbd0cf12b13b488c
Titus Muthomi <muthomititus+github@gmail.com>
2022-11-17 10:27:13 +0300

    Add shout and whisper functions

---
+++ index.js
@@ -0,0 +1,13 @@
+// shout(string)
+// 1) receives one argument and returns it in all caps
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+// whisper(string)
+//     1) receives one argument and returns it in all lowercase
+
+function whisper(string) {
+  return string.toLowerCase();
+}
